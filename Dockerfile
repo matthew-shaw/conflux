78f47fc144c042b115a113f375907874021a4669
Matt Shaw <matthew-shaw@users.noreply.github.com>
2025-07-26 16:42:02 +0100

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,17 @@
+FROM python:3.13-slim
+
+RUN useradd appuser
+
+WORKDIR /home/appuser
+
+# Set environment variables
+ENV FLASK_APP=govuk-frontend-flask.py \
+    PYTHONDONTWRITEBYTECODE=1 \
+    PYTHONUNBUFFERED=1
+
+COPY app app
+COPY govuk-frontend-flask.py config.py requirements.txt ./
+RUN pip install -r requirements.txt \
+    && chown -R appuser:appuser ./
+
+USER appuser
---
+++ app/__init__.py
@@ -0,0 +1,67 @@
+from flask import Flask
+from flask_assets import Bundle, Environment
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
+from flask_wtf.csrf import CSRFProtect
+from govuk_frontend_wtf.main import WTFormsHelpers
+from jinja2 import ChoiceLoader, PackageLoader, PrefixLoader
+from werkzeug.middleware.proxy_fix import ProxyFix
+
+from config import Config
+
+assets = Environment()
+csrf = CSRFProtect()
+limiter = Limiter(
+    get_remote_address,
+    default_limits=["2 per second", "60 per minute"],
+)
+
+
+def create_app(config_class=Config):
+    app = Flask(__name__, static_url_path="/assets")
+    app.config.from_object(config_class)
+    app.jinja_env.lstrip_blocks = True
+    app.jinja_env.trim_blocks = True
+    app.jinja_loader = ChoiceLoader(
+        [
+            PackageLoader("app"),
+            PrefixLoader(
+                {
+                    "govuk_frontend_jinja": PackageLoader("govuk_frontend_jinja"),
+                    "govuk_frontend_wtf": PackageLoader("govuk_frontend_wtf"),
+                }
+            ),
+        ]
+    )
+    app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1, x_proto=1)
+
+    # Initialise app extensions
+    assets.init_app(app)
+    csrf.init_app(app)
+    limiter.init_app(app)
+    WTFormsHelpers(app)
+
+    # Create static asset bundles
+    css = Bundle(
+        "src/css/*.css",
+        filters="cssmin",
+        output="dist/css/custom-%(version)s.min.css",
+    )
+    js = Bundle(
+        "src/js/*.js",
+        filters="jsmin",
+        output="dist/js/custom-%(version)s.min.js",
+    )
+    if "css" not in assets:
+        assets.register("css", css)
+    if "js" not in assets:
+        assets.register("js", js)
+
+    # Register blueprints
+    from app.demos import bp as demo_bp
+    from app.main import bp as main_bp
+
+    app.register_blueprint(demo_bp)
+    app.register_blueprint(main_bp)
+
+    return app
