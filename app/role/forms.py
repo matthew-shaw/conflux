26815aa7dcce059843ab5d9401d3fafc0cd83b31
Matt Shaw <matthew.shaw@landregistry.gov.uk>
2025-07-29 23:52:11 +0100

    create new role form and route

---
+++ app/role/forms.py
@@ -0,0 +1,13 @@
+from flask_wtf import FlaskForm
+from govuk_frontend_wtf.wtforms_widgets import GovSubmitInput, GovTextInput
+from wtforms.fields import StringField, SubmitField
+from wtforms.validators import InputRequired
+
+
+class RoleForm(FlaskForm):
+    title = StringField(
+        "Title",
+        widget=GovTextInput(),
+        validators=[InputRequired(message="Enter the title")],
+    )
+    save: SubmitField = SubmitField("Save and continue", widget=GovSubmitInput())
--- app/role/routes.py
+++ app/role/routes.py
@@ -1,8 +1,18 @@
-from flask import render_template
+from flask import flash, redirect, render_template, url_for
 
 from app.role import bp
+from app.role.forms import RoleForm
 
 
 @bp.route("/", methods=["GET", "POST"])
 def list():
     return render_template("list-roles.html")
+
+
+@bp.route("/new", methods=["GET", "POST"])
+def create():
+    form = RoleForm()
+    if form.validate_on_submit():
+        flash(f"You've created the {form.title.data} role", "success")
+        return redirect(url_for("role.list"))
+    return render_template("create-role.html", form=form)
