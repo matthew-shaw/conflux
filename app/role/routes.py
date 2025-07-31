127af355c9d475add9acdd9ccd2ca51acdbc3b1e
Matt Shaw <matthew.shaw@landregistry.gov.uk>
2025-07-31 10:21:07 +0100

    placeholder route handlers

--- app/role/routes.py
+++ app/role/routes.py
@@ -1,10 +1,12 @@
 from flask import flash, redirect, render_template, url_for
 
+from app import db
+from app.models import Role
 from app.role import bp
 from app.role.forms import RoleForm
 
 
-@bp.route("/", methods=["GET", "POST"])
+@bp.route("/", methods=["GET"])
 def list():
     return render_template("list-roles.html")
 
@@ -13,6 +15,21 @@ def list():
 def create():
     form = RoleForm()
     if form.validate_on_submit():
-        flash(f"The {form.title.data} role has been created", "success")
+        flash(f"{form.title.data} has been created", "success")
         return redirect(url_for("role.list"))
     return render_template("create-role.html", form=form)
+
+
+@bp.route("/<uuid:id>", methods=["GET"])
+def view(id):
+    pass
+
+
+@bp.route("/<uuid:id>/edit", methods=["GET", "POST"])
+def edit(id):
+    pass
+
+
+@bp.route("/<uuid:id>/archive", methods=["GET", "POST"])
+def archive(id):
+    pass
