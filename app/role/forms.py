8ac8ba6badd909c268fa02690537322e245842aa
Matt Shaw <matthew.shaw@landregistry.gov.uk>
2025-08-02 00:30:11 +0100

    check for role title uniqueness in form validation

--- app/role/forms.py
+++ app/role/forms.py
@@ -1,7 +1,9 @@
 from flask_wtf import FlaskForm
 from govuk_frontend_wtf.wtforms_widgets import GovSubmitInput, GovTextInput
 from wtforms.fields import StringField, SubmitField
-from wtforms.validators import InputRequired
+from wtforms.validators import InputRequired, ValidationError
+
+from app.models import Role
 
 
 class RoleForm(FlaskForm):
@@ -11,3 +13,8 @@ class RoleForm(FlaskForm):
         validators=[InputRequired(message="Enter the title")],
     )
     save: SubmitField = SubmitField("Save", widget=GovSubmitInput())
+
+    def validate_title(self, field):
+        # Check if a role with this title already exists
+        if Role.query.filter_by(title=field.data).first():
+            raise ValidationError("A role with this title already exists.")
