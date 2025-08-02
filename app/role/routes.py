bd5c6ee1ac6111c0c385f3ef876225b2f59248eb
Matt Shaw <matthew.shaw@landregistry.gov.uk>
2025-08-03 00:25:23 +0100

    use govuk datetime utils

--- app/role/routes.py
+++ app/role/routes.py
@@ -8,7 +8,7 @@ from app import db
 from app.models import Role
 from app.role import bp
 from app.role.forms import RoleForm
-
+from app.utils.govuk_datetime_utils import format_govuk_datetime
 
 @bp.route("/", methods=["GET"])
 def list() -> str:
@@ -18,7 +18,7 @@ def list() -> str:
     rows: List[List[Dict[str, Any]]] = [
         [
             {"html": f'<a href="{url_for("role.view", id=role.id)}" class="govuk-link">{role.title}</a>'},
-            {"text": role.updated_at.strftime("%-d %b %Y")},
+            {"text": format_govuk_datetime(role.updated_at, include_day=False)},
         ]
         for role in roles
     ]
@@ -44,7 +44,9 @@ def create() -> str:
 
 @bp.route("/<uuid:id>", methods=["GET"])
 def view(id: UUID) -> str:
-    return render_template("view-role.html", role=Role.query.get_or_404(id))
+    role = Role.query.get_or_404(id)
+    role.updated_at = format_govuk_datetime(role.updated_at, include_time=True, include_day=False)
+    return render_template("view-role.html", role=role)
 
 
 @bp.route("/<uuid:id>/edit", methods=["GET", "POST"])
