711742d1772fd310676b8e09926198f20bd46b89
Matt Shaw <matthew.shaw@landregistry.gov.uk>
2025-08-07 10:58:34 +0100

    ignore bandit issue

--- config.py
+++ config.py
@@ -31,7 +31,7 @@ class TestConfig(object):
     DEPARTMENT_NAME = "Department of Magical Law Enforcement"
     DEPARTMENT_URL = "https://www.example.com/"
     RATELIMIT_HEADERS_ENABLED = True
-    SECRET_KEY = "4f378500459bb58fecf903ea3c113069f11f150b33388f56fc89f7edce0e6a84"
+    SECRET_KEY = "4f378500459bb58fecf903ea3c113069f11f150b33388f56fc89f7edce0e6a84"  # nosec B105
     SERVICE_NAME = "Apply for a wand licence"
     SERVICE_PHASE = "Beta"
     SESSION_COOKIE_HTTPONLY = True
