a6c9c8333c2aebd1392f0a7d2034854463a7774b
Matt Shaw <matthew.shaw@landregistry.gov.uk>
2025-08-08 15:44:40 +0100

    implement search

--- app/search/forms.py
+++ app/search/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm  # type: ignore
-from govuk_frontend_wtf.wtforms_widgets import GovRadioInput, GovSubmitInput, GovTextInput  # type: ignore
-from wtforms.fields import RadioField, StringField, SubmitField  # type: ignore
+from govuk_frontend_wtf.wtforms_widgets import GovSubmitInput, GovTextInput  # type: ignore
+from wtforms.fields import StringField, SubmitField  # type: ignore
 from wtforms.validators import InputRequired  # type: ignore
 
 
@@ -9,17 +9,5 @@ class SearchForm(FlaskForm):
         "Query",
         widget=GovTextInput(),
         validators=[InputRequired(message="Enter a query")],
-    )
-    type: RadioField = RadioField(
-        "Type",
-        widget=GovRadioInput(),
-        validators=[InputRequired(message="Select a type")],
-        choices=[
-            ("roles", "Roles"),
-            ("people", "People"),
-            ("teams", "Teams"),
-            ("services", "Services"),
-            ("components", "Components"),
-        ],
     )
     submit: SubmitField = SubmitField("Search", widget=GovSubmitInput())
--- app/search/routes.py
+++ app/search/routes.py
@@ -1,5 +1,9 @@
-from flask import render_template
+from typing import List
 
+from flask import render_template, url_for
+
+from app import db
+from app.models import Role
 from app.search import bp
 from app.search.forms import SearchForm
 
@@ -8,7 +12,23 @@ from app.search.forms import SearchForm
 def index() -> str:
     """Render the search page."""
     form: SearchForm = SearchForm()
+    results: List = []
 
     if form.validate_on_submit():
-        pass
-    return render_template("search.html", form=form)
+        query: str = f"%{form.query.data.strip()}%"
+
+        # Search Roles
+        roles = db.session.execute(db.select(Role).filter(Role.name.ilike(query))).scalars().all()
+
+        # Combine all results into a single list with type info
+        for role in roles:
+            results.append(
+                {
+                    "type": "Role",
+                    "id": role.id,
+                    "name": role.name,
+                    "url": url_for("role.view", id=role.id),
+                }
+            )
+
+    return render_template("search.html", form=form, results=results)
