382d41e5bdbb3d203a2c57536caf9aae2a203854
Matt Shaw <matthew.shaw@landregistry.gov.uk>
2025-08-09 23:52:13 +0100

    randomised people data

--- data/load.sh
+++ data/load.sh
@@ -3,6 +3,12 @@ docker compose exec mimisbrunnr psql \
   -U mimir \
   -d mimisbrunnr \
   -c "\
+TRUNCATE TABLE roles, people CASCADE;
+
 COPY roles (id, name, grade, updated_at, archived_at) \
 FROM '/data/roles.csv' \
+WITH (FORMAT csv, HEADER true);
+
+COPY people (id, name, archived_at, updated_at, role_id, team_id) \
+FROM '/data/people.csv' \
 WITH (FORMAT csv, HEADER true);"
