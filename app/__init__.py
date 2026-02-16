9b9311eecc7bef769c28df1d10b4d1eae69b8521
Matt Shaw <matthew.shaw@landregistry.gov.uk>
2026-02-16 19:06:06 +0000

    govuk-frontend v6

--- app/__init__.py
+++ app/__init__.py
@@ -32,7 +32,6 @@ def create_app(config_class: Type[Config] = Config) -> Flask:
     app: Flask = Flask(__name__)  # type: ignore[assignment]
     app.config.from_object(config_class)
     app.jinja_env.globals["format_govuk_datetime"] = format_govuk_datetime
-    app.jinja_env.globals["govukRebrand"] = True
     app.jinja_env.lstrip_blocks = True
     app.jinja_env.trim_blocks = True
 
